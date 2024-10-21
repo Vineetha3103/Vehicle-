d4f135d238491f1f7f7dd089062faa1b72b7e0d0
Vineetha <thurpuvineetha@gmail.com>
2024-10-21 21:07:58 +0530

    success

--- src/app/layout/library/library.component.ts
+++ src/app/layout/library/library.component.ts
@@ -14,8 +14,33 @@ interface Book {
   standalone: true,
   imports: [CommonModule],
   templateUrl: './library.component.html',
-  styleUrl: './library.component.scss'
+  styleUrls: ['./library.component.scss']
 })
 export class LibraryComponent {
   books: Book[] = booksData;
+
+  bookCategories: string[] = [
+    "All",
+    "category Book 1",
+    "category Book 2",
+    "category Book 3",
+    "category Book 4",
+    "category Book 5",
+    "category Book 6",
+    "category Book 7",
+    "category Book 8",
+    "category Book 9"
+  ];
+
+  stars: number[] = [1, 2, 3, 4, 5]; // For looping 5 times to represent stars
+
+  // Method to get the number of full stars based on the book rating
+  getFullStars(rating: string): number {
+    return Math.floor(Number(rating)); // Convert rating to number and get full stars
+  }
+
+  // Method to check if there is a half star
+  hasHalfStar(rating: string): boolean {
+    return (Number(rating) % 1) >= 0.5; // Check if the decimal part is 0.5 or more
+  }
 }
